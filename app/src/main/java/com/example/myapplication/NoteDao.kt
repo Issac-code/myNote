784a08bd26c1797ec2d7d6f610211766e20221d2
Issac-code <eshaqjabbari_dev>
2023-09-08 20:00:54 +0430

    create NoteDao interface

---
+++ app/src/main/java/com/example/myapplication/NoteDao.kt
@@ -0,0 +1,16 @@
+package com.example.myapplication
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+// annotation for dao class.
+@Dao
+interface NotesDao {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(note :Note)
+    @Delete
+    suspend fun delete(note: Note)
+    @Query("Select * from notesTable order by id ASC")
+    fun getAllNotes(): LiveData<List<Note>>
+    @Update
+    suspend fun update(note: Note)
+}
